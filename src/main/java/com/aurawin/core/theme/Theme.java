ad258737d31e8d9427123f941221d118b3cc3407
Andrew <atbrunner@aurawin.com>
2015-09-03 13:39:25 -0500

    Added more theme objects

--- src/main/java/com/aurawin/core/theme/Theme.java
+++ src/main/java/com/aurawin/core/theme/Theme.java
@@ -53,45 +53,88 @@ public class Theme {
         public static class Button{
             public static class Default {
                 public static String Add = "table.light.button.default.add";
+                public static String ArrowheadDown = "table.light.button.default.arrowhead.down";
+                public static String ArrowheadLeft = "table.light.button.default.arrowhead.left";
+                public static String ArrowheadRight = "table.light.button.default.arrowhead.right";
+                public static String ArrowheadUp = "table.light.button.default.arrowhead.up";
                 public static String Delete = "table.light.button.default.delete";
                 public static String New = "table.light.button.default.new";
                 public static String File = "table.light.button.default.file";
                 public static String Folder = "table.light.button.default.folder";
+                public static String List = "table.light.button.default.list";
+                public static String Attribute = "table.light.button.default.attribute";
+                public static String InsertRow = "table.light.button.default.insert-row";
+
             }
             public static class Disabled {
                 public static String Add = "table.light.button.disabled.add";
+                public static String ArrowheadDown = "table.light.button.disabled.arrowhead.down";
+                public static String ArrowheadLeft = "table.light.button.disabled.arrowhead.left";
+                public static String ArrowheadRight = "table.light.button.disabled.arrowhead.right";
+                public static String ArrowheadUp = "table.light.button.disabled.arrowhead.up";
                 public static String Delete = "table.light.button.disabled.delete";
                 public static String New = "table.light.button.disabled.new";
                 public static String File = "table.light.button.disabled.file";
                 public static String Folder = "table.light.button.disabled.folder";
+                public static String List = "table.light.button.disabled.list";
+                public static String Attribute = "table.light.button.disabled.attribute";
+                public static String InsertRow = "table.light.button.disabled.insert-row";
             }
             public static class Pressed{
                 public static String Add = "table.light.button.pressed.add";
+                public static String ArrowheadDown = "table.light.button.pressed.arrowhead.down";
+                public static String ArrowheadLeft = "table.light.button.pressed.arrowhead.left";
+                public static String ArrowheadRight = "table.light.button.pressed.arrowhead.right";
+                public static String ArrowheadUp = "table.light.button.pressed.arrowhead.up";
                 public static String Delete = "table.light.button.pressed.delete";
                 public static String New = "table.light.button.pressed.new";
                 public static String File = "table.light.button.pressed.file";
                 public static String Folder = "table.light.button.pressed.folder";
+                public static String List = "table.light.button.pressed.list";
+                public static String Attribute = "table.light.button.pressed.attribute";
+                public static String InsertRow = "table.light.button.pressed.insert-row";
             }
             public static class Rollover{
                 public static String Add = "table.light.button.rollover.add";
+                public static String ArrowheadDown = "table.light.button.rollover.arrowhead.down";
+                public static String ArrowheadLeft = "table.light.button.rollover.arrowhead.left";
+                public static String ArrowheadRight = "table.light.button.rollover.arrowhead.right";
+                public static String ArrowheadUp = "table.light.button.rollover.arrowhead.up";
                 public static String Delete = "table.light.button.rollover.delete";
                 public static String New = "table.light.button.rollover.new";
                 public static String File = "table.light.button.rollover.file";
                 public static String Folder = "table.light.button.rollover.folder";
+                public static String List = "table.light.button.rollover.list";
+                public static String Attribute = "table.light.button.rollover.attribute";
+                public static String InsertRow = "table.light.button.rollover.insert-row";
             }
             public static class RolloverSelected{
                 public static String Add = "table.light.button.rollover-selected.add";
+                public static String ArrowheadDown = "table.light.button.rollover-selected.arrowhead.down";
+                public static String ArrowheadLeft = "table.light.button.rollover-selected.arrowhead.left";
+                public static String ArrowheadRight = "table.light.button.rollover-selected.arrowhead.right";
+                public static String ArrowheadUp = "table.light.button.rollover-selected.arrowhead.up";
                 public static String Delete = "table.light.button.rollover-selected.delete";
                 public static String New = "table.light.button.rollover-selected.new";
                 public static String File = "table.light.button.rollover-selected.file";
-                public static String Foldder = "table.light.button.rollover-selected.folder";
+                public static String Folder = "table.light.button.rollover-selected-selected.folder";
+                public static String List = "table.light.button.rollover-selected.list";
+                public static String Attribute = "table.light.button.rollover-selected.attribute";
+                public static String InsertRow = "table.light.button.rollover-selected.insert-row";
             }
             public static class Selected{
                 public static String Add = "table.light.button.selected.add";
+                public static String ArrowheadDown = "table.light.button.selected.arrowhead.down";
+                public static String ArrowheadLeft = "table.light.button.selected.arrowhead.left";
+                public static String ArrowheadRight = "table.light.button.selected.arrowhead.right";
+                public static String ArrowheadUp = "table.light.button.selected.arrowhead.up";
                 public static String Delete = "table.light.button.selected.delete";
                 public static String New = "table.light.button.selected.new";
                 public static String File = "table.light.button.selected.file";
                 public static String Folder = "table.light.button.selected.folder";
+                public static String List = "table.light.button.selected.list";
+                public static String Attribute = "table.light.button.selected.attribute";
+                public static String InsertRow = "table.light.button.selected.insert-row";
             }
         }
 
