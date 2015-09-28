5ad18bc02d1363dc5f0879628918ece64d1d064a
Andrew <atbrunner@aurawin.com>
2015-09-28 16:42:32 -0500

    Remvoed references to scs stored objects

--- src/main/java/com/aurawin/core/stored/Hibernate.java
+++ src/main/java/com/aurawin/core/stored/Hibernate.java
@@ -2,7 +2,6 @@ package com.aurawin.core.stored;
 
 import com.aurawin.core.lang.Table;
 import com.aurawin.core.stored.entities.Stored;
-import com.aurawin.core.stored.entities.domain.network.Network;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
